8ba30d1b0e146899a4aa76920dea4fedb7050cfe
dgt41 <d.grammatiko@gmail.com>
2015-02-03 02:47:56 +0200

    Tooltip follows selection

--- administrator/components/com_media/controller.php
+++ administrator/components/com_media/controller.php
@@ -66,10 +66,6 @@ class MediaController extends JControllerLegacy
 
 		// Get/Create the view
 		$view = $this->getView($vName, $vType);
-		if (JFactory::getApplication()->isSite())
-		{
-			$view->addTemplatePath(JPATH_COMPONENT_ADMINISTRATOR . '/views/' . strtolower($vName) . '/tmpl');
-		}
 
 		// Get/Create the model
 		if ($model = $this->getModel($mName))
