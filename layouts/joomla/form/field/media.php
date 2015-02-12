52d38cde36678254e9e8bb5fe161f40928b1f949
dgt41 <d.grammatiko@gmail.com>
2015-02-12 15:33:15 +0200

    Use the mootools version as default for B/C

--- layouts/joomla/form/field/media.php
+++ layouts/joomla/form/field/media.php
@@ -32,22 +32,45 @@ defined('_JEXEC') or die;
  */
 extract($displayData);
 
-JHtml::_('bootstrap.modal');
+// Load the modal behavior script.
+JHtml::_('behavior.modal');
 
-// The button.
-if ($disabled != true)
+// Include jQuery
+JHtml::_('jquery.framework');
+
+// Tooltip for INPUT showing whole image path
+$options = array(
+	'onShow' => 'jMediaRefreshImgpathTip',
+);
+
+JHtml::_('behavior.tooltip', '.hasTipImgpath', $options);
+
+if (!empty($class))
 {
-	JHtml::_('bootstrap.tooltip');
+	$class .= ' hasTipImgpath';
+}
+else
+{
+	$class = 'hasTipImgpath';
 }
 
 $attr = '';
 
+$attr .= ' title="' . htmlspecialchars('<span id="TipImgpath"></span>', ENT_COMPAT, 'UTF-8') . '"';
+
 // Initialize some field attributes.
-$attr .= !empty($class) ? ' class="input-small hasTooltip ' . $class . '"' : ' class="input-small hasTooltip"';
+$attr .= !empty($class) ? ' class="input-small ' . $class . '"' : ' class="input-small"';
 $attr .= !empty($size) ? ' size="' . $size . '"' : '';
 
 // Initialize JavaScript field attributes.
 $attr .= !empty($onchange) ? ' onchange="' . $onchange . '"' : '';
+
+// The text field.
+echo '<div class="input-prepend input-append">';
+
+// The Preview.
+$showPreview = true;
+$showAsTooltip = false;
 
 switch ($preview)
 {
@@ -55,16 +78,19 @@ switch ($preview)
 	case 'false':
 	case 'none':
 		$showPreview = false;
-		$showAsTooltip = false;
 		break;
+
 	case 'yes': // Deprecated parameter value
 	case 'true':
 	case 'show':
 		break;
 	case 'tooltip':
 	default:
-		$showPreview = true;
 		$showAsTooltip = true;
+		$options = array(
+				'onShow' => 'jMediaRefreshPreviewTip',
+		);
+		JHtml::_('behavior.tooltip', '.hasTipPreview', $options);
 		break;
 }
 
@@ -77,62 +103,60 @@ if ($showPreview)
 	}
 	else
 	{
-		$src = JText::_('JLIB_FORM_MEDIA_PREVIEW_EMPTY');
+		$src = '';
+	}
+
+	$width = $previewWidth;
+	$height = $previewHeight;
+	$style = '';
+	$style .= ($width > 0) ? 'max-width:' . $width . 'px;' : '';
+	$style .= ($height > 0) ? 'max-height:' . $height . 'px;' : '';
+
+	$imgattr = array(
+		'id' => $id . '_preview',
+		'class' => 'media-preview',
+		'style' => $style,
+	);
+
+	$img = JHtml::image($src, JText::_('JLIB_FORM_MEDIA_PREVIEW_ALT'), $imgattr);
+	$previewImg = '<div id="' . $id . '_preview_img"' . ($src ? '' : ' style="display:none"') . '>' . $img . '</div>';
+	$previewImgEmpty = '<div id="' . $id . '_preview_empty"' . ($src ? ' style="display:none"' : '') . '>'
+		. JText::_('JLIB_FORM_MEDIA_PREVIEW_EMPTY') . '</div>';
+
+	if ($showAsTooltip)
+	{
+		echo '<div class="media-preview add-on">';
+		$tooltip = $previewImgEmpty . $previewImg;
+		$options = array(
+			'title' => JText::_('JLIB_FORM_MEDIA_PREVIEW_SELECTED_IMAGE'),
+					'text' => '<i class="icon-eye"></i>',
+					'class' => 'hasTipPreview'
+					);
+
+		echo JHtml::tooltip($tooltip, $options);
+		echo '</div>';
+	}
+	else
+	{
+		echo '<div class="media-preview add-on" style="height:auto">';
+		echo ' ' . $previewImgEmpty;
+		echo ' ' . $previewImg;
+		echo '</div>';
 	}
 }
 
-// The url for the modal
-$url = ($readonly ? ''
+echo '	<input type="text" name="' . $name . '" id="' . $id . '" value="'
+	. htmlspecialchars($value, ENT_COMPAT, 'UTF-8') . '" readonly="readonly"' . $attr . ' />';
+
+?>
+<a class="modal btn" title="<?php echo JText::_('JLIB_FORM_BUTTON_SELECT'); ?>" href="
+<?php echo ($readonly ? ''
 		: ($link ? $link
 		: 'index.php?option=com_media&amp;view=images&amp;tmpl=component&amp;asset=' . $asset . '&amp;author='
-		. $authorId) . '&amp;fieldid=' . $id . '&amp;folder=' . $folder) . '"';
+	. $authorField) . '&amp;fieldid=' . $id . '&amp;folder=' . $folder) . '"'
+	. ' rel="{handler: \'iframe\', size: {x: 800, y: 500}}"'; ?>>
+ <?php echo JText::_('JLIB_FORM_BUTTON_SELECT'); ?></a><a class="btn hasTooltip" title="<?php echo JText::_('JLIB_FORM_BUTTON_CLEAR'); ?>" href="#" onclick="jInsertFieldValue('', '<?php echo $id; ?>'); return false;">
+	<i class="icon-remove"></i></a>
 
-// Render the modal
-echo JHtmlBootstrap::renderModal(
-						'imageModal_'. $id, array(
-							'url' => $url,
-							'title' => JText::_('JLIB_FORM_CHANGE_IMAGE'),
-							'width' => '800px',
-							'height' => '565px')
-						);
 
-/*
- * Add javascript for:
- * proxy jModalClose to close the modal
- * initialize popover
- */
-JFactory::getDocument()->addScriptDeclaration('
-	jQuery(document).ready(function(){
-		if ("' . $src . '" === "' . JText::_('JLIB_FORM_MEDIA_PREVIEW_EMPTY') . '") {
-			imagePreview = "' . JText::_('JLIB_FORM_MEDIA_PREVIEW_EMPTY') . '";
-		} else {
-			var imagePreview = new Image(' .$previewWidth .', ' .$previewHeight .');
-			imagePreview.src = "' . $src . '";
-		}
-		jQuery("#media_preview_' . $id . '").popover({trigger: "hover", placement: "right", content: imagePreview, html: true});
-	});
-');
-?>
-<?php if ($showPreview) : ?>
-<div class="input-prepend input-append">
-	<div class="media-preview add-on" style="padding: 0; border: 0;">
-		<span id="media_preview_<?php echo $id; ?>" rel="popover" class="btn" title="<?php echo
-		JText::_('JLIB_FORM_MEDIA_PREVIEW_SELECTED_IMAGE'); ?>" data-content="" data-original-title="<?php
-		echo JText::_('JLIB_FORM_MEDIA_PREVIEW_SELECTED_IMAGE'); ?>" data-trigger="hover">
-		<i class="icon-eye"></i>
-		</span>
-	</div>
-<? endif; ?>
-<?php if (!$showPreview) : ?>
-<div class="input-append">
-<? endif; ?>
-	<input type="text" name="<?php echo $name; ?>" id="<?php echo $id; ?>" value="<?php echo htmlspecialchars($value, ENT_COMPAT, 'UTF-8'); ?>" readonly="readonly"<?php echo $attr; ?>/>
-
-<?php if ($disabled != true) : ?>
-	<a href="#imageModal_<?php echo $id; ?>" role="button" class="btn" data-toggle="modal"><?php echo JText::_("JLIB_FORM_BUTTON_SELECT"); ?></a>
-
-	<a class="btn hasTooltip" title="<?php echo JText::_("JLIB_FORM_BUTTON_CLEAR"); ?>" href="#" onclick="jInsertFieldValue('', '<?php echo $id; ?>');">
-		<i class="icon-remove"></i>
-	</a>
-<?php endif; ?>
 </div>
