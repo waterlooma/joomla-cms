d419e3fea1a430fb398638ef55c67eed02b89a9c
dgt41 <d.grammatiko@gmail.com>
2015-02-12 04:54:22 +0200

    simplify things

--- administrator/components/com_media/controller.php
+++ administrator/components/com_media/controller.php
@@ -29,51 +29,39 @@ class MediaController extends JControllerLegacy
 	public function display($cachable = false, $urlparams = false)
 	{
 		JPluginHelper::importPlugin('content');
-		$vName = $this->input->get('view', 'media');
-
 		$document = JFactory::getDocument();
+		$vName    = $this->input->get('view', 'media');
 		$vType    = $document->getType();
 
 		switch ($vName)
 		{
 			case 'images':
 				$vLayout = $this->input->get('layout', 'default', 'string');
-				$mName = 'manager';
-				// Get/Create the view
-				$view = $this->getView($vName, $vType);
-				if (JFactory::getApplication()->isSite() && !file_exists(JPATH_ROOT . '/templates/' . JFactory::getApplication()->getTemplate() .
-					'/html/com_media/images/default.php'))
-				{
-					$view->addTemplatePath($this->basePath . '/views/' . strtolower($vName) . '/tmpl');
-				}
+				$mName   = 'manager';
 				break;
 
 			case 'imagesList':
 				$mName   = 'list';
 				$vLayout = $this->input->get('layout', 'default', 'string');
-				// Get/Create the view
-				$view = $this->getView($vName, $vType);
-				$view->addTemplatePath($this->basePath . '/views/' . strtolower($vName) . '/tmpl');
 				break;
 
 			case 'mediaList':
 				$app     = JFactory::getApplication();
 				$mName   = 'list';
 				$vLayout = $app->getUserStateFromRequest('media.list.layout', 'layout', 'thumbs', 'word');
-				// Get/Create the view
-				$view = $this->getView($vName, $vType);
 				break;
 
 			case 'media':
 			default:
 				$vName   = 'media';
 				$vLayout = $this->input->get('layout', 'default', 'string');
-				$mName = 'manager';
-				// Get/Create the view
-				$view = $this->getView($vName, $vType);
-				$view->addTemplatePath($this->basePath . '/views/' . strtolower($vName) . '/tmpl');
+				$mName   = 'manager';
 				break;
 		}
+
+		// Get/Create the view
+		$config['base_path'] = $this->basePath;
+		$view = $this->getView($vName, $vType, '', $config);
 
 		// Get/Create the model
 		if ($model = $this->getModel($mName))
