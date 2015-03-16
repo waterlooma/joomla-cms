e1fba595c27d6107dc4c5126a7809146dc3745f6
George Wilson <georgejameswilson@googlemail.com>
2015-03-16 14:47:07 +0000

    Set up variables necessary for JApplicationWeb

--- tests/unit/suites/libraries/cms/language/JLanguageMultilangTest.php
+++ tests/unit/suites/libraries/cms/language/JLanguageMultilangTest.php
@@ -7,6 +7,8 @@
  * @license     GNU General Public License version 2 or later; see LICENSE
  */
 
+use Joomla\Registry\Registry;
+
 /**
  * Test class for JLanguageMultiLang.
  *
@@ -16,6 +18,83 @@
  */
 class JLanguageMultiLangTest extends TestCaseDatabase
 {
+	/**
+	 * Value for test host.
+	 *
+	 * @var    string
+	 * @since  3.2
+	 */
+	const TEST_HTTP_HOST = 'mydomain.com';
+
+	/**
+	 * Value for test user agent.
+	 *
+	 * @var    string
+	 * @since  3.2
+	 */
+	const TEST_USER_AGENT = 'Mozilla/5.0';
+
+	/**
+	 * Value for test user agent.
+	 *
+	 * @var    string
+	 * @since  3.2
+	 */
+	const TEST_REQUEST_URI = '/index.php';
+
+	/**
+	 * Backup of the SERVER superglobal
+	 *
+	 * @var    array
+	 * @since  3.4
+	 */
+	protected $backupServer;
+
+	/**
+	 * Setup for testing.
+	 *
+	 * @return  void
+	 *
+	 * @since   3.2
+	 */
+	public function setUp()
+	{
+		parent::setUp();
+
+		$this->saveFactoryState();
+
+		JFactory::$document = $this->getMockDocument();
+		JFactory::$language = $this->getMockLanguage();
+		JFactory::$session  = $this->getMockSession();
+
+		$this->backupServer = $_SERVER;
+
+		$_SERVER['HTTP_HOST'] = self::TEST_HTTP_HOST;
+		$_SERVER['HTTP_USER_AGENT'] = self::TEST_USER_AGENT;
+		$_SERVER['REQUEST_URI'] = self::TEST_REQUEST_URI;
+		$_SERVER['SCRIPT_NAME'] = '/index.php';
+	}
+
+	/**
+	 * Overrides the parent tearDown method.
+	 *
+	 * @return  void
+	 *
+	 * @see     PHPUnit_Framework_TestCase::tearDown()
+	 * @since   3.2
+	 */
+	protected function tearDown()
+	{
+		// Reset the dispatcher instance.
+		TestReflection::setValue('JEventDispatcher', 'instance', null);
+
+		$_SERVER = $this->backupServer;
+		$this->restoreFactoryState();
+
+		parent::tearDown();
+	}
+
+
 	/**
 	 * Gets the data set to be loaded into the database during setup
 	 *
