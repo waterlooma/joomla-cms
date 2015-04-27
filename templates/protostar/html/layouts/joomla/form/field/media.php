226842eb99b23a35423ad586c25fa17c520f6bf2
dgt41 <d.grammatiko@gmail.com>
2015-04-27 20:03:54 +0300

    oops

--- templates/protostar/html/layouts/joomla/form/field/media.php
+++ templates/protostar/html/layouts/joomla/form/field/media.php
@@ -1,7 +1,7 @@
 <?php
 /**
  * @package     Joomla.Site
- * @subpackage  Layout
+ * @subpackage  com_media
  *
  * @copyright   Copyright (C) 2005 - 2015 Open Source Matters, Inc. All rights reserved.
  * @license     GNU General Public License version 2 or later; see LICENSE.txt
@@ -9,215 +9,136 @@
 
 defined('_JEXEC') or die;
 
-/**
- * Layout variables
- * ---------------------
- *
- * @var  string   $asset The asset text
- * @var  string   $authorField The label text
- * @var  integer  $authorId The author id
- * @var  string   $class The class text
- * @var  boolean  $disabled True if field is disabled
- * @var  string   $folder The folder text
- * @var  string   $id The label text
- * @var  string   $link The link text
- * @var  string   $name The name text
- * @var  string   $preview The preview image relative path
- * @var  integer  $previewHeight The image preview height
- * @var  integer  $previewWidth The image preview width
- * @var  string   $onchange  The onchange text
- * @var  boolean  $readonly True if field is readonly
- * @var  integer  $size The size text
- * @var  string   $value The value text
- */
-extract($displayData);
+JHtml::_('formbehavior.chosen', 'select');
 
-// Load the modal behavior script.
-JHtml::_('behavior.modal');
+// Load tooltip instance without HTML support because we have a HTML tag in the tip
+JHtml::_('bootstrap.tooltip', '.noHtmlTip', array('html' => false));
 
-// Include jQuery
-JHtml::_('jquery.framework');
+$user   = JFactory::getUser();
+$input  = JFactory::getApplication()->input;
+$params = JComponentHelper::getParams('com_media');
 
-JFactory::getDocument()->addStyleDeclaration(
-	'
-		function jInsertFieldValue(value, id) {
-		var $ = jQuery.noConflict();
-		var old_value = $("#" + id).val();
-		if (old_value != value) {
-			var $elem = $("#" + id);
-			$elem.val(value);
-			$elem.trigger("change");
-			if (typeof($elem.get(0).onchange) === "function") {
-				$elem.get(0).onchange();
-			}
-			jMediaRefreshPreview(id);
-		}
-	}
-
-	function jMediaRefreshPreview(id) {
-		var $ = jQuery.noConflict();
-		var value = $("#" + id).val();
-		var $img = $("#" + id + "_preview");
-		if ($img.length) {
-			if (value) {
-				$img.attr("src", "' . JUri::root() . '" + value);
-				$("#" + id + "_preview_empty").hide();
-				$("#" + id + "_preview_img").show()
-			} else {
-				$img.attr("src", "")
-				$("#" + id + "_preview_empty").show();
-				$("#" + id + "_preview_img").hide();
-			}
-		}
-	}
-
-	function jMediaRefreshPreviewTip(tip)
-	{
-		var $ = jQuery.noConflict();
-		var $tip = $(tip);
-		var $img = $tip.find("img.media-preview");
-		$tip.find("div.tip").css("max-width", "none");
-		var id = $img.attr("id");
-		id = id.substring(0, id.length - "_preview".length);
-		jMediaRefreshPreview(id);
-		$tip.show();
-	}
-
-	// JQuery for tooltip for INPUT showing whole image path
-	function jMediaRefreshImgpathTip(tip)
-	{
-		var $ = jQuery.noConflict();
-		var $tip = $(tip);
-		$tip.css("max-width", "none");
-		var $imgpath = $("#" + "' . $id . '").val();
-		$("#TipImgpath").html($imgpath);
-		if ($imgpath.length) {
-		 $tip.show();
-		} else {
-		 $tip.hide();
-		}
-	}
-	'
-);
-// Tooltip for INPUT showing whole image path
-$options = array(
-	'onShow' => 'jMediaRefreshImgpathTip',
-);
-
-JHtml::_('behavior.tooltip', '.hasTipImgpath', $options);
-
-if (!empty($class))
-{
-	$class .= ' hasTipImgpath';
-}
-else
-{
-	$class = 'hasTipImgpath';
-}
-
-$attr = '';
-
-$attr .= ' title="' . htmlspecialchars('<span id="TipImgpath"></span>', ENT_COMPAT, 'UTF-8') . '"';
-
-// Initialize some field attributes.
-$attr .= !empty($class) ? ' class="input-small ' . $class . '"' : ' class="input-small"';
-$attr .= !empty($size) ? ' size="' . $size . '"' : '';
-
-// Initialize JavaScript field attributes.
-$attr .= !empty($onchange) ? ' onchange="' . $onchange . '"' : '';
-
-// The text field.
-echo '<div class="input-prepend input-append">';
-
-// The Preview.
-$showPreview = true;
-$showAsTooltip = false;
-
-switch ($preview)
-{
-	case 'no': // Deprecated parameter value
-	case 'false':
-	case 'none':
-		$showPreview = false;
-		break;
-
-	case 'yes': // Deprecated parameter value
-	case 'true':
-	case 'show':
-		break;
-	case 'tooltip':
-	default:
-		$showAsTooltip = true;
-		$options = array(
-				'onShow' => 'jMediaRefreshPreviewTip',
-		);
-		JHtml::_('behavior.tooltip', '.hasTipPreview', $options);
-		break;
-}
-
-// Pre fill the contents of the popover
-if ($showPreview)
-{
-	if ($value && file_exists(JPATH_ROOT . '/' . $value))
-	{
-		$src = JUri::root() . $value;
-	}
-	else
-	{
-		$src = '';
-	}
-
-	$width = $previewWidth;
-	$height = $previewHeight;
-	$style = '';
-	$style .= ($width > 0) ? 'max-width:' . $width . 'px;' : '';
-	$style .= ($height > 0) ? 'max-height:' . $height . 'px;' : '';
-
-	$imgattr = array(
-		'id' => $id . '_preview',
-		'class' => 'media-preview',
-		'style' => $style,
-	);
-
-	$img = JHtml::image($src, JText::_('JLIB_FORM_MEDIA_PREVIEW_ALT'), $imgattr);
-	$previewImg = '<div id="' . $id . '_preview_img"' . ($src ? '' : ' style="display:none"') . '>' . $img . '</div>';
-	$previewImgEmpty = '<div id="' . $id . '_preview_empty"' . ($src ? ' style="display:none"' : '') . '>'
-		. JText::_('JLIB_FORM_MEDIA_PREVIEW_EMPTY') . '</div>';
-
-	if ($showAsTooltip)
-	{
-		echo '<div class="media-preview add-on">';
-		$tooltip = $previewImgEmpty . $previewImg;
-		$options = array(
-			'title' => JText::_('JLIB_FORM_MEDIA_PREVIEW_SELECTED_IMAGE'),
-					'text' => '<i class="icon-eye"></i>',
-					'class' => 'hasTipPreview'
-					);
-
-		echo JHtml::tooltip($tooltip, $options);
-		echo '</div>';
-	}
-	else
-	{
-		echo '<div class="media-preview add-on" style="height:auto">';
-		echo ' ' . $previewImgEmpty;
-		echo ' ' . $previewImg;
-		echo '</div>';
-	}
-}
-
-echo '	<input type="text" name="' . $name . '" id="' . $id . '" value="'
-	. htmlspecialchars($value, ENT_COMPAT, 'UTF-8') . '" readonly="readonly"' . $attr . ' />';
-
+// fieldid refers to input and e_name refers to editor image button
+JFactory::getDocument()->addScriptDeclaration("
+var image_base_path = '" . $params->get('image_path', 'images') . "/';
+	function bsMediaModalClose(){
+		parent.jQuery('#imageModal_" . $input->getCmd('fieldid') . "', parent.document).modal('hide');
+		parent.jQuery('#imageModal_" . $input->getCmd('e_name') . "', parent.document).modal('hide');
+	};
+");
 ?>
-<a class="modal btn" title="<?php echo JText::_('JLIB_FORM_BUTTON_SELECT'); ?>" href="
-<?php echo ($readonly ? ''
-		: ($link ? $link
-		: 'index.php?option=com_media&amp;view=images&amp;tmpl=component&amp;asset=' . $asset . '&amp;author='
-	. $authorField) . '&amp;fieldid=' . $id . '&amp;folder=' . $folder) . '"'
-	. ' rel="{handler: \'iframe\', size: {x: 800, y: 500}}"'; ?>>
- <?php echo JText::_('JLIB_FORM_BUTTON_SELECT'); ?></a><a class="btn hasTooltip" title="<?php echo JText::_('JLIB_FORM_BUTTON_CLEAR'); ?>" href="#" onclick="jInsertFieldValue('', '<?php echo $id; ?>'); return false;">
-	<i class="icon-remove"></i></a>
+	<form action="index.php?option=com_media&amp;asset=<?php echo $input->getCmd('asset');?>&amp;author=<?php echo $input->getCmd('author'); ?>" class="form-vertical" id="imageForm" method="post" enctype="multipart/form-data">
+		<div id="messages" style="display: none;">
+			<span id="message"></span><?php echo JHtml::_('image', 'media/dots.gif', '...', array('width' => 22, 'height' => 12), true) ?>
+		</div>
+		<div class="well">
+			<div class="row">
+				<div class="span12 control-group">
+					<div class="control-label">
+						<label class="control-label" for="folder"><?php echo JText::_('COM_MEDIA_DIRECTORY') ?></label>
+					</div>
+					<div class="controls">
+						<?php echo $this->folderList; ?>
+						<button class="btn" type="button" id="upbutton" title="<?php echo JText::_('COM_MEDIA_DIRECTORY_UP') ?>"><?php echo JText::_('COM_MEDIA_UP') ?></button>
+					</div>
+				</div>
+			</div>
+		</div>
 
+		<iframe id="imageframe" name="imageframe" src="index.php?option=com_media&amp;view=imagesList&amp;tmpl=component&amp;folder=<?php echo $this->state->folder?>&amp;asset=<?php echo $input->getCmd('asset');?>&amp;author=<?php echo $input->getCmd('author');?>"></iframe>
 
-</div>
+		<div class="well">
+			<div class="row">
+				<div class="span6 control-group">
+					<div class="control-label">
+						<label for="f_url"><?php echo JText::_('COM_MEDIA_IMAGE_URL') ?></label>
+					</div>
+					<div class="controls">
+						<input type="text" id="f_url" value="" />
+					</div>
+				</div>
+				<?php if (!$this->state->get('field.id')):?>
+					<div class="span6 control-group">
+						<div class="control-label">
+							<label title="<?php echo JText::_('COM_MEDIA_ALIGN_DESC'); ?>" class="noHtmlTip" for="f_align"><?php echo JText::_('COM_MEDIA_ALIGN') ?></label>
+						</div>
+						<div class="controls">
+							<select size="1" id="f_align">
+								<option value="" selected="selected"><?php echo JText::_('COM_MEDIA_NOT_SET') ?></option>
+								<option value="left"><?php echo JText::_('JGLOBAL_LEFT') ?></option>
+								<option value="center"><?php echo JText::_('JGLOBAL_CENTER') ?></option>
+								<option value="right"><?php echo JText::_('JGLOBAL_RIGHT') ?></option>
+							</select>
+						</div>
+					</div>
+				<?php endif;?>
+			</div>
+			<?php if (!$this->state->get('field.id')):?>
+				<div class="row">
+					<div class="span6 control-group">
+						<div class="control-label">
+							<label for="f_alt"><?php echo JText::_('COM_MEDIA_IMAGE_DESCRIPTION') ?></label>
+						</div>
+						<div class="controls">
+							<input type="text" id="f_alt" value="" />
+						</div>
+					</div>
+					<div class="span6 control-group">
+						<div class="control-label">
+							<label for="f_title"><?php echo JText::_('COM_MEDIA_TITLE') ?></label>
+						</div>
+						<div class="controls">
+							<input type="text" id="f_title" value="" />
+						</div>
+					</div>
+				</div>
+				<div class="row">
+					<div class="span6 control-group">
+						<div class="control-label">
+							<label for="f_caption"><?php echo JText::_('COM_MEDIA_CAPTION') ?></label>
+						</div>
+						<div class="controls">
+							<input type="text" id="f_caption" value="" />
+						</div>
+					</div>
+					<div class="span6 control-group">
+						<div class="control-label">
+							<label title="<?php echo JText::_('COM_MEDIA_CAPTION_CLASS_DESC'); ?>" class="noHtmlTip" for="f_caption_class"><?php echo JText::_('COM_MEDIA_CAPTION_CLASS_LABEL') ?></label>
+						</div>
+						<div class="controls">
+							<input type="text" list="d_caption_class" id="f_caption_class" value="" />
+							<datalist id="d_caption_class">
+								<option value="text-left">
+								<option value="text-center">
+								<option value="text-right">
+							</datalist>
+						</div>
+					</div>
+				</div>
+			<?php endif;?>
+
+			<input type="hidden" id="dirPath" name="dirPath" />
+			<input type="hidden" id="f_file" name="f_file" />
+			<input type="hidden" id="tmpl" name="component" />
+
+		</div>
+	</form>
+
+<?php if ($user->authorise('core.create', 'com_media')) : ?>
+	<form action="<?php echo JUri::base(); ?>index.php?option=com_media&amp;task=file.upload&amp;tmpl=component&amp;<?php echo $this->session->getName() . '=' . $this->session->getId(); ?>&amp;<?php echo JSession::getFormToken();?>=1&amp;asset=<?php echo $input->getCmd('asset');?>&amp;author=<?php echo $input->getCmd('author');?>&amp;view=images" id="uploadForm" class="form-horizontal" name="uploadForm" method="post" enctype="multipart/form-data">
+		<div id="uploadform" class="well">
+			<fieldset id="upload-noflash" class="actions">
+				<div class="control-group">
+					<div class="control-label">
+						<label for="upload-file" class="control-label"><?php echo JText::_('COM_MEDIA_UPLOAD_FILE'); ?></label>
+					</div>
+					<div class="controls">
+						<input type="file" id="upload-file" name="Filedata[]" multiple /><button class="btn btn-primary" id="upload-submit"><i class="icon-upload icon-white"></i> <?php echo JText::_('COM_MEDIA_START_UPLOAD'); ?></button>
+						<p class="help-block"><?php echo $this->config->get('upload_maxsize') == '0' ? JText::_('COM_MEDIA_UPLOAD_FILES_NOLIMIT') : JText::sprintf('COM_MEDIA_UPLOAD_FILES', $this->config->get('upload_maxsize')); ?></p>
+					</div>
+				</div>
+			</fieldset>
+			<?php JFactory::getSession()->set('com_media.return_url', 'index.php?option=com_media&view=images&tmpl=component&fieldid=' . $input->getCmd('fieldid', '') . '&e_name=' . $input->getCmd('e_name') . '&asset=' . $input->getCmd('asset') . '&author=' . $input->getCmd('author')); ?>
+		</div>
+	</form>
+<?php endif;
