b4e23768fbfa317b2fc0558c948d75fdd1ca57cf
dgt41 <d.grammatiko@gmail.com>
2015-05-21 02:47:13 +0300

    init

--- administrator/components/com_banners/views/banners/tmpl/default.php
+++ administrator/components/com_banners/views/banners/tmpl/default.php
@@ -23,6 +23,7 @@ $canOrder	= $user->authorise('core.edit.state', 'com_banners.category');
 $archived	= $this->state->get('filter.state') == 2 ? true : false;
 $trashed	= $this->state->get('filter.state') == -2 ? true : false;
 $saveOrder	= $listOrder == 'a.ordering';
+$published = $this->state->get('filter.published');
 
 if ($saveOrder)
 {
@@ -183,9 +184,20 @@ if ($saveOrder)
 					<?php endforeach; ?>
 				</tbody>
 			</table>
+			<?php //Load the batch processing form. ?>
+			<?php echo JHtml::_(
+				'bootstrap.renderModal',
+				'collapseModal',
+				array(
+					'title' => JText::_('COM_BANNERS_BATCH_OPTIONS'),
+					'width' => '800px',
+					'height' => '300px',
+					'footer' => $this->loadTemplate('batch_footer')
+				),
+				$this->loadTemplate('batch_body')
+			);
+			?>
 		<?php endif; ?>
-		<?php //Load the batch processing form. ?>
-		<?php echo $this->loadTemplate('batch'); ?>
 
 		<input type="hidden" name="task" value="" />
 		<input type="hidden" name="boxchecked" value="0" />
