0137e81262cfd1f22c1747258c0f05e6e74ae45a
dgt41 <d.grammatiko@gmail.com>
2015-05-27 01:26:33 +0300

    batch modals adj

--- administrator/templates/hathor/html/com_categories/categories/default.php
+++ administrator/templates/hathor/html/com_categories/categories/default.php
@@ -13,7 +13,6 @@ defined('_JEXEC') or die;
 JHtml::addIncludePath(JPATH_COMPONENT . '/helpers/html');
 
 JHtml::_('behavior.multiselect');
-JHtml::_('behavior.modal');
 
 $app		= JFactory::getApplication();
 $user		= JFactory::getUser();
@@ -192,7 +191,19 @@ $assoc		= JLanguageAssociations::isEnabled();
 			<div class="clr"></div>
 
 			<?php //Load the batch processing form. ?>
-			<?php echo $this->loadTemplate('batch'); ?>
+			<?php if ($user->authorise('core.create', $extension)
+				&& $user->authorise('core.edit', $extension)
+				&& $user->authorise('core.edit.state', $extension)) : ?>
+				<?php echo JHtml::_(
+					'bootstrap.renderModal',
+					'collapseModal',
+					array(
+						'title' => JText::_('COM_CATEGORIES_BATCH_OPTIONS'),
+						'footer' => $this->loadTemplate('batch_footer')
+					),
+					$this->loadTemplate('batch_body')
+				); ?>
+			<?php endif; ?>
 
 			<input type="hidden" name="extension" value="<?php echo $extension; ?>" />
 			<input type="hidden" name="task" value="" />
