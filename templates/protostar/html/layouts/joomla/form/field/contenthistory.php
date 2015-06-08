2dedd2d6238fcd0f6ad0f0a99152e7f27b708005
dgt41 <d.grammatiko@gmail.com>
2015-06-08 16:51:40 +0300

    JCLOSE doesn't exist

--- templates/protostar/html/layouts/joomla/form/field/contenthistory.php
+++ templates/protostar/html/layouts/joomla/form/field/contenthistory.php
@@ -28,7 +28,7 @@ echo JHtml::_(
 		'height' => '300px',
 		'width' => '800px',
 		'footer' => '<button class="btn" data-dismiss="modal" aria-hidden="true">'
-			. JText::_("JCLOSE") . '</button>'
+			. JText::_("JLIB_HTML_BEHAVIOR_CLOSE") . '</button>'
 	)
 );
 
