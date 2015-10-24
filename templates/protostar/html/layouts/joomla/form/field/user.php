9a518c4ce412ea076c381feea2785a9192705fe3
dgt41 <d.grammatiko@gmail.com>
2015-10-24 20:53:19 +0300

    Ensure compatibility with repeatable

--- templates/protostar/html/layouts/joomla/form/field/user.php
+++ templates/protostar/html/layouts/joomla/form/field/user.php
@@ -27,6 +27,7 @@ extract($displayData);
  * @var string           $userName The user name
  * @var boolean          $readOnly Check for field read only attribute
  */
+
 // Set the link for the user selection page
 $link = 'index.php?option=com_users&amp;view=users&amp;layout=modal&amp;tmpl=component&amp;field=' . $id
 	. (isset($groups) ? ('&amp;groups=' . base64_encode(json_encode($groups))) : '')
@@ -35,30 +36,34 @@ $link = 'index.php?option=com_users&amp;view=users&amp;layout=modal&amp;tmpl=com
 JHtml::script('jui/fielduser.min.js', false, true, false, false, true);
 ?>
 <?php // Create a dummy text field with the user name. ?>
-<div class="input-append">
-	<input
-		type="text" id="<?php echo $id; ?>_name"
-		value="<?php echo  htmlspecialchars($userName, ENT_COMPAT, 'UTF-8'); ?>"
-		readonly
-		disabled="disabled"
-		<?php echo $class ? ' class="' . (string) $class . '"' : ''; ?>
-		<?php echo $size ? ' size="' . (int) $size . '"' : ''; ?>/>
-	<?php if (!$readOnly) : ?>
-		<a href="#userModal" role="button" class="btn btn-primary" data-toggle="modal" title="<?php echo JText::_('JLIB_FORM_CHANGE_USER') ?>"><i class="icon-user"></i></a>
-		<?php echo JHtml::_(
-			'bootstrap.renderModal',
-			'userModal',
-			array(
-				'url'    => $link,
-				'title'  => JText::_('JLIB_FORM_CHANGE_USER'),
-				'height' => '300px',
-				'width'  => '800px',
-				'footer' => '<button class="btn" data-dismiss="modal" aria-hidden="true">'
-					. JText::_("JLIB_HTML_BEHAVIOR_CLOSE") . '</button>'
-			)
-		); ?>
-	<?php endif; ?>
+<div class="field-user-wrapper"
+	data-url="<?php echo $link; ?>"
+	data-modal=".modal"
+	data-modal-width="100%"
+	data-modal-height="400px"
+	data-input=".field-user-input"
+	data-button-select=".button-select"
+	>
+	<div class="input-append">
+		<input
+			type="text" id="<?php echo $id; ?>_name"
+			value="<?php echo  htmlspecialchars($userName, ENT_COMPAT, 'UTF-8'); ?>"
+			readonly
+			disabled="disabled"
+			<?php echo $class ? ' class="' . (string) $class . '"' : ''; ?>
+			<?php echo $size ? ' size="' . (int) $size . '"' : ''; ?>/>
+		<?php if (!$readOnly) : ?>
+			<a class="btn btn-primary button-select" title="<?php echo JText::_('JLIB_FORM_CHANGE_USER') ?>"><span class="icon-user"></span></a>
+			<?php echo JHtml::_(
+				'bootstrap.renderModal',
+				'userModal_' . $id,
+				array(
+					'title'  => JText::_('JLIB_FORM_CHANGE_USER'),
+					'closeButton' => true
+				)
+			); ?>
+		<?php endif; ?>
+	</div>
+	<?php // Create the real field, hidden, that stored the user id. ?>
+	<input type="hidden" id="<?php echo $id; ?>_id" name="<?php echo $name; ?>" value="<?php echo (int) $value; ?>" data-onchange="<?php echo $this->escape($onchange); ?>"/>
 </div>
-
-<?php // Create the real field, hidden, that stored the user id. ?>
-<input type="hidden" id="<?php echo $id; ?>_id" name="<?php echo $name; ?>" value="<?php echo (int) $value; ?>" data-onchange="<?php echo $this->escape($onchange); ?>"/>
