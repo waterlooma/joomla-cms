bf16ce40d241e077957e358ddf7a48985b7799b7
dgt41 <d.grammatiko@gmail.com>
2015-11-20 16:24:54 +0200

    Mootools are evil 😈

---
+++ templates/protostar/html/layouts/joomla/editors/buttons.php
@@ -0,0 +1,48 @@
+<?php
+/**
+ * @package     Joomla.Site
+ * @subpackage  Layout
+ *
+ * @copyright   Copyright (C) 2005 - 2015 Open Source Matters, Inc. All rights reserved.
+ * @license     GNU General Public License version 2 or later; see LICENSE.txt
+ */
+
+defined('_JEXEC') or die;
+
+$buttons = $displayData;
+
+// Override jModalClose and SqueezeBox.close for B/C
+JFactory::getDocument()->addScriptDeclaration(
+		"
+		function jModalClose() {
+			SqueezeBox.close();
+			jQuery('.modal, .in ').modal('hide');
+		}
+
+		var SqueezeBox;
+		if (SqueezeBox != undefined)
+		{
+			bsClose = function(){
+				jQuery('.modal, .in ').modal('hide');
+			}
+			SqueezeBox.close.bind(bsClose);
+		} else {
+			var SqueezeBox = {};
+			SqueezeBox.close = function(){
+			jQuery('.modal, .in ').modal('hide');
+			}
+		}
+		"
+);
+
+?>
+<div id="editor-xtd-buttons" class="btn-toolbar pull-left">
+	<?php if ($buttons) : ?>
+		<?php foreach ($buttons as $button) : ?>
+			<?php echo JLayoutHelper::render('joomla.editors.buttons.button', $button); ?>
+		<?php endforeach; ?>
+		<?php foreach ($buttons as $button) : ?>
+			<?php echo JLayoutHelper::render('joomla.editors.buttons.modal', $button); ?>
+		<?php endforeach; ?>
+	<?php endif; ?>
+</div>
