03ff1d6c9bf08db6b71f0060363f7166ffd418bd
George Wilson <georgejameswilson@googlemail.com>
2016-03-14 23:48:00 +0000

    Fix docblocks

--- administrator/components/com_newsfeeds/helpers/newsfeeds.php
+++ administrator/components/com_newsfeeds/helpers/newsfeeds.php
@@ -43,11 +43,11 @@ class NewsfeedsHelper extends JHelperContent
 	/**
 	 * Adds Count Items for Category Manager.
 	 *
-	 * @param   JDatabaseQuery  &$items  The item object of com_categories
+	 * @param   JObject[]  &$items  The banner category objects
 	 *
-	 * @return  JDatabaseQuery
+	 * @return  JObject[]
 	 *
-	 * @since   3.4
+	 * @since   3.5
 	 */
 	public static function countItems(&$items)
 	{
